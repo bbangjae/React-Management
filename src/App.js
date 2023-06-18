ffedcdf04b91e350214fa78fe8301e4b5e3f794f
bbangjae <thebyunjae@naver.com>
2023-06-18 23:10:35 +0900

    Create Customer js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import Customer from './components/Customer';
 import './App.css';
 
-const customer = {
+const customers = [{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
   'name':'홍길동',
   'birthday':'970503'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'영재',
+  'birthday':'972503'
 }
+]
 function App() {
   return (
-    <Customer 
-      name={customer.name}
-      birthday={customer.birthday}
+    <div>
+      {
+        customers.map(c=> {
+          return <Customer 
+            key={c.id} // 키값을 주어야함 (구별짓는)
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+
+          />
+        })
+      }
+    {/* <Customer 
+      id={customers[0].id}
+      image={customers[0].image}
+      name={customers[0].name}
+      birthday={customers[0].birthday}
     />
-  );
+    <Customer 
+      id={customers[1].id}
+      image={customers[1].image}
+      name={customers[1].name}
+      birthday={customers[1].birthday}
+    /> */}
+    </div>
+  ); 
 }
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,12 +4,32 @@ class Customer extends React.Component{
     render(){//실제로 화면에 그려지는걸 담는다는 의미
         return( // render함수는 꼭 리턴을 넣어줘야함
         <div>
-            <h2> {this.props.name}</h2>
-            <p>{this.props.birthday}</p>
+            <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
+            <CustomerInfo birthday={this.props.birthday} />
         </div>
         )
     }
 
+}
+class CustomerProfile extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.image} alt="profile" />
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+
+}
+class CustomerInfo extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>{this.props.birthday}</p>
+            </div>
+        )
+    }
 }
 
 
